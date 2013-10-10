eafc823713720fa90429a7133250b5a4af21b9c4
nickgian <nick.giannarakis@gmail.com>
2013-10-10 14:14:40 +0300

    split to ml and mli

---
+++ semantic/Quads.mli
@@ -0,0 +1,115 @@
+(** Infrastructure for creation of the Quads IR *)
+
+module Label :
+  sig
+    type t = int
+    val label : t ref
+    val newLabel : unit -> t
+    val nextLabel : unit -> t
+  end
+module type LABEL_LIST =
+  sig
+    type labelList = Label.t list
+    val newLabelList : unit -> labelList
+    val makeLabelList : Label.t -> labelList
+    val addLabel : Label.t -> labelList -> labelList
+    val is_empty : labelList -> bool
+    exception EmptyLabelList
+    val removeLabel : labelList -> Label.t * labelList
+    val peekLabel : labelList -> Label.t
+    val mergeLabels : labelList -> labelList -> labelList
+  end
+module Labels : LABEL_LIST
+type quad_operators =
+    Q_Unit
+  | Q_Endu
+  | Q_Plus
+  | Q_Minus
+  | Q_Mult
+  | Q_Div
+  | Q_Mod
+  | Q_Fplus
+  | Q_Fminus
+  | Q_Fmult
+  | Q_Fdiv
+  | Q_L
+  | Q_Le
+  | Q_G
+  | Q_Ge
+  | Q_Seq
+  | Q_Nseq
+  | Q_Eq
+  | Q_Neq
+  | Q_Assign
+  | Q_Ifb
+  | Q_Array
+  | Q_Jump
+  | Q_Jumpl
+  | Q_Label
+  | Q_Call
+  | Q_Par
+  | Q_Ret
+  | Q_Dim
+  | Q_Match
+  | Q_Constr
+  | Q_Fail
+type quad_operands =
+    O_Int of int
+  | O_Float of float
+  | O_Char of string
+  | O_Bool of bool
+  | O_Str of string
+  | O_Backpatch
+  | O_Label of int
+  | O_Res
+  | O_Ret
+  | O_ByVal
+  | O_Entry of SymbTypes.entry
+  | O_Empty
+  | O_Ref of quad_operands
+  | O_Deref of quad_operands
+  | O_Size of int
+  | O_Dims of int
+  | O_Index of quad_operands list
+type quad = {
+  label : Label.t;
+  operator : quad_operators;
+  arg1 : quad_operands;
+  arg2 : quad_operands;
+  mutable arg3 : quad_operands;
+}
+type expr_info = { place : quad_operands; next_expr : Labels.labelList; }
+type cond_info = {
+  true_lst : Labels.labelList;
+  false_lst : Labels.labelList;
+}
+type stmt_info = { next_stmt : Labels.labelList; }
+val labelsTbl : (int, int) Hashtbl.t
+val memLabelTbl : int -> bool
+val addLabelTbl : int -> unit
+val newTemp : Types.typ -> SymbTypes.entry -> quad_operands
+val getQuadBop : AstTypes.binop -> quad_operators
+val getQuadUnop : AstTypes.unop -> quad_operators
+val getQuadOpType : quad_operands -> Types.typ
+val newQuadList : unit -> 'a list
+val isEmptyQuadList : 'a list -> bool
+val genQuad :
+  quad_operators * quad_operands * quad_operands * quad_operands ->
+  quad list -> quad list
+val mergeQuads : 'a list -> 'a list -> 'a list
+val setExprInfo : quad_operands -> Labels.labelList -> expr_info
+val setCondInfo : Labels.labelList -> Labels.labelList -> cond_info
+val setStmtInfo : Labels.labelList -> stmt_info
+val backpatch : quad list -> Label.t list -> int -> quad list
+val auxil_funs : (string * SymbTypes.entry) list
+val findAuxilEntry : string -> SymbTypes.entry
+val is_auxil_fun : Identifier.id -> bool
+val string_of_operator : quad_operators -> string
+val print_operator : Format.formatter -> quad_operators -> unit
+val print_entry : Format.formatter -> SymbTypes.entry -> unit
+val print_indexes : Format.formatter -> quad_operands list -> unit
+val print_operand : Format.formatter -> quad_operands -> unit
+val entry_of_quadop : quad_operands -> SymbTypes.entry
+val normalizeQuads : quad list -> quad list
+val printQuad : Format.formatter -> quad -> unit
+val printQuads : Format.formatter -> quad list -> unit
---
+++ semantic/cfg.mli
@@ -0,0 +1,155 @@
+(** Creates the control flow graph on the quads intermediate form. *)
+
+module LabelSet :
+  sig
+    type elt = Quads.Label.t
+    type t
+    val empty : t
+    val is_empty : t -> bool
+    val mem : elt -> t -> bool
+    val add : elt -> t -> t
+    val singleton : elt -> t
+    val remove : elt -> t -> t
+    val union : t -> t -> t
+    val inter : t -> t -> t
+    val diff : t -> t -> t
+    val compare : t -> t -> int
+    val equal : t -> t -> bool
+    val subset : t -> t -> bool
+    val iter : (elt -> unit) -> t -> unit
+    val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
+    val for_all : (elt -> bool) -> t -> bool
+    val exists : (elt -> bool) -> t -> bool
+    val filter : (elt -> bool) -> t -> t
+    val partition : (elt -> bool) -> t -> t * t
+    val cardinal : t -> int
+    val elements : t -> elt list
+    val min_elt : t -> elt
+    val max_elt : t -> elt
+    val choose : t -> elt
+    val split : elt -> t -> t * bool * t
+    val find : elt -> t -> elt
+  end
+
+module LS :
+  sig
+    include module type of LabelSet
+    (** [print_set s] prints the elements (in this case labels) of 
+       [s] *)
+    val print_set : t -> string
+  end
+
+module Blocks :
+  sig
+  include module type of List
+    type bblock = Quads.quad list
+    type block_elt =
+        SymbTypes.entry option * SymbTypes.entry option * LS.t * bblock
+    (** An element of type [block_elt] is a quadraple of 
+      {!SymbTypes.entry} [option] holding an optional entry if 
+      a {!Quads.quad_operators} [Q_Unit] is present in the current block, 
+      likewise for a [Q_Endu], a set {!Cfg.LabelSet.t} with the labels of the 
+      quads in the block and finally the block of quads. *)
+    type blocks = block_elt list
+    val create_blocks : bblock -> blocks
+    (** [create_blocks q] takes a list of {!Quads.quad} [[q1; ...;qn]]
+       and creates the basic blocks as defined by type {!blocks}. *)
+    val is_if : Quads.quad_operators -> bool
+    (** Checks whether the operator is a relational operator *)
+    val is_jmp : Quads.quad_operators -> bool
+    (** Checks whether the operator is a jump operator *)
+    val is_call : Quads.quad_operators -> bool
+    (** Checks if the operator is a call operator *)
+    val print_blocks : blocks -> unit
+    (** Prints the basic blocks as calculated by {!create_blocks}. *)
+  end
+
+module V :
+  sig
+    type t = Blocks.bblock
+    val compare : t -> t -> int
+    val hash : t -> int
+    val equal : t -> t -> bool
+  end
+
+module G :
+  sig
+    type t = Graph.Persistent.Digraph.Concrete(V).t
+    module V :
+      sig
+        type t = V.t
+        val compare : t -> t -> int
+        val hash : t -> int
+        val equal : t -> t -> bool
+        type label = V.t
+        val create : label -> t
+        val label : t -> label
+      end
+    type vertex = V.t
+    module E :
+      sig
+        type t = V.t * V.t
+        val compare : t -> t -> int
+        val src : t -> vertex
+        val dst : t -> vertex
+        type label = unit
+        val create : vertex -> label -> vertex -> t
+        val label : t -> label
+      end
+    type edge = E.t
+    val is_directed : bool
+    val is_empty : t -> bool
+    val nb_vertex : t -> int
+    val nb_edges : t -> int
+    val out_degree : t -> vertex -> int
+    val in_degree : t -> vertex -> int
+    val mem_vertex : t -> vertex -> bool
+    val mem_edge : t -> vertex -> vertex -> bool
+    val mem_edge_e : t -> edge -> bool
+    val find_edge : t -> vertex -> vertex -> edge
+    val find_all_edges : t -> vertex -> vertex -> edge list
+    val succ : t -> vertex -> vertex list
+    val pred : t -> vertex -> vertex list
+    val succ_e : t -> vertex -> edge list
+    val pred_e : t -> vertex -> edge list
+    val iter_vertex : (vertex -> unit) -> t -> unit
+    val fold_vertex : (vertex -> 'a -> 'a) -> t -> 'a -> 'a
+    val iter_edges : (vertex -> vertex -> unit) -> t -> unit
+    val fold_edges : (vertex -> vertex -> 'a -> 'a) -> t -> 'a -> 'a
+    val iter_edges_e : (edge -> unit) -> t -> unit
+    val fold_edges_e : (edge -> 'a -> 'a) -> t -> 'a -> 'a
+    val map_vertex : (vertex -> vertex) -> t -> t
+    val iter_succ : (vertex -> unit) -> t -> vertex -> unit
+    val iter_pred : (vertex -> unit) -> t -> vertex -> unit
+    val fold_succ : (vertex -> 'a -> 'a) -> t -> vertex -> 'a -> 'a
+    val fold_pred : (vertex -> 'a -> 'a) -> t -> vertex -> 'a -> 'a
+    val iter_succ_e : (edge -> unit) -> t -> vertex -> unit
+    val fold_succ_e : (edge -> 'a -> 'a) -> t -> vertex -> 'a -> 'a
+    val iter_pred_e : (edge -> unit) -> t -> vertex -> unit
+    val fold_pred_e : (edge -> 'a -> 'a) -> t -> vertex -> 'a -> 'a
+    val empty : t
+    val add_vertex : t -> vertex -> t
+    val remove_vertex : t -> vertex -> t
+    val add_edge : t -> vertex -> vertex -> t
+    val add_edge_e : t -> edge -> t
+    val remove_edge : t -> vertex -> vertex -> t
+    val remove_edge_e : t -> edge -> t
+  end
+
+module CFG :
+  sig
+    include module type of G
+    type cfg = G.t
+    type flow = G.edge
+    type vblock = G.vertex
+    val create_cfg : Quads.quad list -> cfg
+    val print_vertices : G.t -> unit
+  end
+
+module Dot :  
+  sig    
+    val fprint_graph :      
+      Format.formatter -> Graph.Persistent.Digraph.Concrete(V).t -> unit    
+    val output_graph :             
+      out_channel -> Graph.Persistent.Digraph.Concrete(V).t -> unit          
+  end   
